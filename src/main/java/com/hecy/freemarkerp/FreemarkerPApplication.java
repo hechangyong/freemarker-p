d8327657fa5c48ece96e558d2abdae39b31a9dbd
hecy <hechangyong@aliyun.com>
2018-10-26 18:22:34 +0800

    添加初始化项目

---
+++ src/main/java/com/hecy/freemarkerp/FreemarkerPApplication.java
@@ -0,0 +1,12 @@
+package com.hecy.freemarkerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarkerPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemarkerPApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hecy/freemarkerp/FreemarkerPApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hecy.freemarkerp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreemarkerPApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
